531ac6d65b843574db3fa9f97a2ada06dbbf6f17
Ethan Bonsignori <ebonsignori@gmail.com>
2021-03-17 17:31:09 -0400

    basic layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
+import React from 'react';
+
 function App() {
   return (
     <div className="App">
-     <h1>Profile Picture Upload</h1>
+      <div className="wrapper">
+      <h3>Profile Picture Upload</h3>
+      <img className="profile-picture" alt="profile" src="https://placehold.it/200x200"/>
+      <div className="button-wrapper">
+        <button>Upload</button>
+        <button>Delete</button>
+      </div>
+     </div>
     </div>
   );
 };
