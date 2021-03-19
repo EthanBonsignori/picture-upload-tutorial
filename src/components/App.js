c2cfe92694de363852b988e4a3ec38a497f7b7f9
Ethan Bonsignori <ebonsignori@gmail.com>
2021-03-18 20:23:44 -0400

    add file upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,39 @@
 import React from 'react';
 
 function App() {
+
+  // Placeholder, will fetch url from Database when setup
+  const profilePictureUrl = "https://placehold.it/200x200"
+
+  const handleChange = (evt) => {
+    const reader = new FileReader();
+    const file = evt.target.files[0];
+
+    reader.readAsDataURL(file);
+
+    // when the file is converted
+    reader.onload = () => {
+      console.log(reader.result)
+    }
+
+    // log errors
+    reader.onerror = (err) => {
+      console.log(err)
+    }
+  }
+
   return (
     <div className="App">
-      <div className="wrapper">
       <h3>Profile Picture Upload</h3>
-      <img className="profile-picture" alt="profile" src="https://placehold.it/200x200"/>
-      <div className="button-wrapper">
-        <button>Upload</button>
-        <button>Delete</button>
-      </div>
-     </div>
+      <img className="profile-picture" alt="profile" src={profilePictureUrl} />
+      <label htmlFor="upload" className="upload-label">Change Profile Picture</label>
+      <input // file upload
+        type="file"
+        accept="image/*"
+        id="upload"
+        onChange={handleChange}
+        style={{ display: 'none' }}
+      />
     </div>
   );
 };
